4fca2a8017a26a9a58822cbf6cd0c83cf0918163
NANDHINIR25 <51688500+NANDHINIR25@users.noreply.github.com>
2019-06-12 12:08:20 +0530

    Create alpha.py

---
+++ codekata/alpha.py
@@ -0,0 +1,6 @@
+char=input()
+if(char=='A' or char=='B'or char=='C' or char=='D' or char=='E' or char=='F' or char=='G' or char=='H' or char=='I'or char=='J' or char=='K' or char=='L'or char=='M'or char=='N' or char=='O'or char=='P'  or char=='Q' or char=='R'or char=='S' or char=='T' or char=='U'or char=='V'or char=='W' or char=='X' or char=='Y' or char=='Z'
+or char=='a' or char=='b'or char=='c' or char=='d' or char=='e' or char=='f' or char=='g' or char=='h' or char=='i'or char=='j' or char=='k' or char=='l'or char=='m'or char=='n' or char=='o'or char=='p'  or char=='q' or char=='r' or char=='s' or char=='t' or char=='u'or char=='v'or char=='w'or char=='x' or char=='y' or char=='z'):
+  print("Alphabet")
+else:
+  print("No")
