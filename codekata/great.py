851f563ae7a914b4fe0f2a3e8376e90db73ffee1
NANDHINIR25 <51688500+NANDHINIR25@users.noreply.github.com>
2019-06-12 14:43:37 +0530

    Update great.py

--- codekata/great.py
+++ codekata/great.py
@@ -1,9 +1,8 @@
-ip1=int(input())
-ip2=int(input())
-ip3=int(input())
-if(ip1>=ip2 and ip1>=ip3):
-   print(ip1)
-elif(ip2>=ip3 and ip2>=ip1):
-   print(ip2)
+x,y,z=map(int,input().split())
+if (x>y):
+  if(x>z):
+    print(x)
+elif(y>z):
+  print(y)
 else:
-   print(ip3)
+  print(z)
