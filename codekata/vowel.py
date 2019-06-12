798d319a92e4ae5afc6b3c7d9e79202278d78baf
NANDHINIR25 <51688500+NANDHINIR25@users.noreply.github.com>
2019-06-12 11:51:05 +0530

    Create vowel.py

---
+++ codekata/vowel.py
@@ -0,0 +1,7 @@
+vow=input()
+if(vow=='A' or vow=='E'or vow=='I' or vow=='O' or vow=='U' or vow=='a' or vow=='e' or vow=='i' or vow=='o'or vow=='u'):
+  print("Vowel")
+elif(vow=='!' or vow=='@' or vow=='#'or vow=='$'or vow=="%"or vow=='^'or vow=='&'or vow=='*'):
+  print("invalid")
+else:
+  print("Consonant")
