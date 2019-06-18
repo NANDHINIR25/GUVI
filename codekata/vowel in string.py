192971abe73dab04a4f7b53ef31886e0fe5130be
NANDHINIR25 <51688500+NANDHINIR25@users.noreply.github.com>
2019-06-18 20:52:27 +0530

    Create vowel in string.py

---
+++ codekata/vowel in string.py
@@ -0,0 +1,8 @@
+s=input()
+for i in range(0,len(s)):
+    if s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u' or s[i]=='A' or s[i]=='E' or s[i]=='I' or s[i]=='O'or s[i]=='U':
+       
+           print("yes")
+           break;
+else:
+    print("no")
