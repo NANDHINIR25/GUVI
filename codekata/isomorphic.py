eefc0056a6dbd29092ed4658b80c84ebd5163cbc
NANDHINIR25 <51688500+NANDHINIR25@users.noreply.github.com>
2019-06-21 11:26:36 +0530

    Update isomorphic.py

--- codekata/isomorphic.py
+++ codekata/isomorphic.py
@@ -1,5 +1,9 @@
-ip1,ip2=input().split()
-x=ip1.title()
-y=ip2.title()
-print(x,end=' ')
-print(y)
+str1, str2 = input().split()
+co = 0
+for i in range(len(str1)):
+  if str1.count(str1[i]) == str2.count(str2[i]):
+    co += 1
+if co == len(str1):
+  print ("yes")
+else:
+  print ("no")
