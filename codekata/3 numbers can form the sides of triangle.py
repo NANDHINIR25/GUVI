0b630934daee92c118a107e26bd86dbd7c1c9be5
NANDHINIR25 <51688500+NANDHINIR25@users.noreply.github.com>
2019-07-02 21:53:29 +0530

    Update 3 numbers can form the sides of triangle.py

--- codekata/3 numbers can form the sides of triangle.py
+++ codekata/3 numbers can form the sides of triangle.py
@@ -1,5 +1,5 @@
 ip1,ip2,ip3=map(int,input().split())
-if(((ip1+ip2)>ip3) and ((ip2+ip3)>ip1) and ((ip1+ip3)>ip2)):
+if(((ip1+ip2)>=ip3) and ((ip2+ip3)>=ip1) and ((ip1+ip3)>=ip2)):
   print("yes")
 else:
   print("no")
